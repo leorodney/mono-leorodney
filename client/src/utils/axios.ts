9a0af2d69661f284baf95adec79255b0151c20d1
whybe7 <whybe7@hotmail.com>
2023-05-28 16:15:49 +0100

    global axios configuration

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,13 +2,19 @@ import axios from "axios";
 
 // creating a new axios instance with default configs
 export const server = axios.create({
-    baseURL: `${process.env.VITE_SERVER}:${process.env.VITE_SERVER_PORT}`,
+    baseURL: `${import.meta.env.VITE_SERVER}:${import.meta.env.VITE_SERVER_PORT}`,
+    timeout: 5000,
+    headers:{
+        "Content-Type": "application/json"
+    },
     withCredentials: true,
 });
 
 export const endpoints = {
+    auth: "/",
     login: "/login",
     register: "/register",
+    logout: "/logout",
     promptify: "/promptify",
     production: "/production",
     prompts: "/prompts",
