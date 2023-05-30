8ee6235ce718ab25281bc429ab59d9e910ea5d6b
whybe7 <whybe7@hotmail.com>
2023-05-30 19:03:27 +0100

    update the prompt model to holde bot user id (uid) and the author (username) referneces

--- model/Prompt.ts
+++ model/Prompt.ts
@@ -1,7 +1,8 @@
 import { Schema, model } from 'mongoose';
 
 const Prompt = new Schema({
-    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    uid: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    author: { type: String, ref: "User", required: true },
     value: { type: String, required: true },
     img: { type: String, required: true },
     likes: { type: Number, default: 0 },
